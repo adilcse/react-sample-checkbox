867870983806360a8117323efab2a3fca84a0dd9
adilcse <adil.cs.work@gmail.com>
2022-03-25 15:38:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
+const initialState = () => {
+  const obj = {};
+  [...Array(5).keys()].forEach(key => obj[key]= false);
+  return obj;
+}
 function App() {
+  const [checked, setChacked] = useState(initialState());
+  const [isRed, setIsRed] = useState(true);
+  const handleOnChange = (i) => {
+    setChacked(old => ({...old, [i]: !old[i]}));
+  }
+  useEffect(() => {
+    const checkedBoxes = Object.values(checked).filter(key => key).length;
+    console.log(checkedBoxes)
+    if(checkedBoxes === 0 || checkedBoxes === 5) {
+      setIsRed(true);
+    } else {
+      setIsRed(false);
+    }
+  }, [checked]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" style={{ width:100, height:100, backgroundColor: isRed ? 'red': 'yellow' }}/>
+      <div className="checkboxes">
+      {[...Array(5).keys()].map(i => <>
+        <input
+          type="checkbox"
+          id="topping"
+          name="topping"
+          value="Paneer"
+          key={i}
+          checked={checked[i]}
+          onChange={() =>handleOnChange(i)}
+        />
+        <label for="topping">Checkbox {i+1}</label>
+      </>)}
+      </div>
     </div>
   );
 }
